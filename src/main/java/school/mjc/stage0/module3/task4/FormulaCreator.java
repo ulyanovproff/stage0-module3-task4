d2182f18f339bebd18f3139cd6d12d6ae786a82d
Pavel Ulyanov <ulyanovproff@gmail.com>
2023-09-14 22:53:05 +0500

    commit test

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        int formula = ((9 * a *a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
+
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.module3.task4;
 
+
+
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        System.out.println((char)99);
+        System.out.println((char)104);
+        System.out.println((char)97);
+        System.out.println((char)114);
+
+
+
+
+
     }
 }
